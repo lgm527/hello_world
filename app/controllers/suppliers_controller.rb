c6b827f23ca3bd538988250f00845742f4d44723
lgm527 <laurellmccaffrey@gmail.com>
2020-09-28 12:59:55 -0400

    model validations and controller actions added

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -1,4 +1,29 @@
 class SuppliersController < ApplicationController
-    validates :name, :ein, :address_1, :city, :state, :phone_number, presence: true
-    validates_uniqueness_of :ein
+    def new
+        @supplier = Supplier.new
+    end
+
+    def create
+        @supplier = Supplier.new(supplier_params)
+        if @supplier.valid?
+            @supplier.save
+            redirect_to @supplier
+        else
+            render :new
+        end
+    end
+
+    def index
+        @suppliers = Supplier.all
+    end
+
+    def show
+        @supplier = Supplier.find(params[:id])
+    end
+
+    private
+
+    def supplier_params
+        params.require(:supplier).permit(:name, :ein, :address_1, :address_2, :city, :state, :phone_number)
+    end
 end
